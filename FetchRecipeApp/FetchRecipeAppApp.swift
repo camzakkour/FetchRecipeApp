f3eab09c22b7483e7b186f7313887502547863b6
Cameron Zakkour <camzakkour@gmail.com>
2023-10-30 15:09:20 -0700

    Initial Commit

---
+++ FetchRecipeApp/FetchRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeAppApp.swift
+//  FetchRecipeApp
+//
+//  Created by Cameron Zakkour on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
